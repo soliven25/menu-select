532f63f4ce5d1ab9bc970ea03bbc2d48d9f06ba6
Sungkeun Choi <soliven25@gmail.com>
2016-05-10 09:28:16 +0900

    add .

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
+// views is directory for all template files.
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
